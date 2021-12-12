59159d3ade2dc6d5e5c37930bff54adef18b7af0
mariusztymowicz <mariusz.tymowicz@gmail.com>
2021-12-12 08:59:58 +0100

    2.2 done

--- src/pl/edu/wsiz/io11/part2/Employee.java
+++ src/pl/edu/wsiz/io11/part2/Employee.java
@@ -4,4 +4,14 @@ public class Employee {
     String firstname;
     String lastname;
     byte age;
+
+    void print() {
+        System.out.println(firstname + " " + lastname + " " + age);
+    }
+
+    static void print(Employee employee) {
+        employee.print();
+        // System.out.println(employee.firstname + " " + employee.lastname + " " + employee.age);
+    }
+
 }
--- src/pl/edu/wsiz/io11/part2/Program.java
+++ src/pl/edu/wsiz/io11/part2/Program.java
@@ -4,9 +4,11 @@ public class Program {
     public static void main(String[] args) {
 
         Employee employee = new Employee();
-
         employee.firstname = "Jan";
         employee.lastname = "Kowalski";
-        employee.age = 30;
+        employee.age = 31;
+
+        employee.print();
+        Employee.print(employee);
     }
 }
